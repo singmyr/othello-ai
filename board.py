043dab90f949015e357f99f6ebf96ba7f359fa45
Singmyr <mattias@singmyr.io>
2018-02-19 01:20:42 +0100

    Adds a random name for the bot.\nFixes some issues with the "find valid moves"-algorithm

--- board.py
+++ board.py
@@ -2,10 +2,19 @@ class Board:
     def __init__(self):
         self.board = [[0] * 8 for i in range(8)]
         self.board[3][3] = 2
-        self.board[3][4] = 2
-        self.board[4][3] = 2
-        self.board[4][4] = 1
-        print('created board')
+        self.board[3][4] = 1
+        self.board[4][3] = 1
+        self.board[4][4] = 2
+        #self.board = [
+        #    [0, 0, 0, 0, 0, 0, 0, 0],
+        #    [0, 0, 0, 0, 0, 0, 0, 0],
+        #    [0, 0, 2, 1, 0, 0, 0, 0],
+        #    [0, 0, 2, 1, 1, 0, 0, 0],
+        #    [0, 0, 2, 1, 2, 0, 0, 0],
+        #    [0, 0, 0, 1, 0, 0, 0, 0],
+        #    [0, 0, 0, 0, 0, 0, 0, 0],
+        #    [0, 0, 0, 0, 0, 0, 0, 0]
+        #]
 
     def __str__(self):
         return str(self.board)
@@ -39,34 +48,34 @@ class Board:
         board = self.board
 
         def check_north(_x, _y):
-            return True if _y > 0 and board[_y - 1][_x] > 0 else False
+            return True if _y > 0 and board[_y - 1][_x] > 0 and board[_y - 1][_x] != player_id else False
 
         def check_nort_east(_x, _y):
-            return True if _x < 6 and _y > 0 and board[_y - 1][_x + 1] > 0 else False
+            return True if _x < 6 and _y > 0 and board[_y - 1][_x + 1] > 0 and board[_y - 1][_x + 1] != player_id else False
 
         def check_east(_x, _y):
-            return True if _x < 6 and board[_y][_x + 1] > 0 else False
+            return True if _x < 6 and board[_y][_x + 1] > 0 and board[_y][_x + 1] != player_id else False
 
         def check_south_east(_x, _y):
-            return True if _x < 6 and _y < 6 and board[_y + 1][_x + 1] > 0 else False
+            return True if _x < 6 and _y < 6 and board[_y + 1][_x + 1] > 0 and board[_y + 1][_x + 1] != player_id else False
 
         def check_south(_x, _y):
-            return True if _y < 6 and board[_y + 1][_x] > 0 else False
+            return True if _y < 6 and board[_y + 1][_x] > 0 and board[_y + 1][_x] != player_id else False
 
         def check_south_west(_x, _y):
-            return True if _x > 0 and _y < 6 and board[_y + 1][_x - 1] > 0 else False
+            return True if _x > 0 and _y < 6 and board[_y + 1][_x - 1] > 0 and board[_y + 1][_x - 1] != player_id else False
 
         def check_west(_x, _y):
-            return True if _x > 0 and board[_y][_x - 1] > 0 else False
+            return True if _x > 0 and board[_y][_x - 1] > 0 and board[_y][_x - 1] != player_id else False
 
         def check_north_west(_x, _y):
-            return True if _x > 0 and _y > 0 and board[_y - 1][_x - 1] > 0 else False
+            return True if _x > 0 and _y > 0 and board[_y - 1][_x - 1] > 0 and board[_y - 1][_x - 1] != player_id else False
 
         potentials = []
 
         for _y, row in enumerate(self.board):
             for _x, col in enumerate(row):
-                if (check_north(_x, _y) or
+                if col == 0 and (check_north(_x, _y) or
                    check_nort_east(_x, _y) or
                    check_east(_x, _y) or
                    check_south_east(_x, _y) or
@@ -85,6 +94,7 @@ class Board:
 
     def slot_has_valid_move(self, pos_x, pos_y, player_id):
         # Check north
+        #print('Checking north')
         if pos_y > 1:
             opponent_found = False
             for _y in reversed(range(0, pos_y, 1)):
@@ -93,8 +103,11 @@ class Board:
                     else False
                 if opponent_found and self.board[_y][pos_x] == player_id:
                     return True
+                if self.board[_y][pos_x] == 0 or self.board[_y][pos_x] == player_id:
+                    break
 
         # Check north east
+        #print('Checking north east')
         if pos_x < 6 and pos_y > 1:
             opponent_found = False
             for _x, _y in zip(range(pos_x + 1, 7, 1), reversed(range(0, pos_y, 1))):
@@ -103,8 +116,11 @@ class Board:
                     else False
                 if opponent_found and self.board[_y][_x] == player_id:
                     return True
+                if self.board[_y][_x] == 0 or self.board[_y][_x] == player_id:
+                    break
 
         # Check east
+        #print('Checking east')
         if pos_x < 6:
             opponent_found = False
             for _x in range(pos_x + 1, 7, 1):
@@ -113,8 +129,11 @@ class Board:
                     else False
                 if opponent_found and self.board[pos_y][_x] == player_id:
                     return True
+                if self.board[pos_y][_x] == 0 or self.board[pos_y][_x] == player_id:
+                    break
 
         # Check south east
+        #print('Checking south east')
         if pos_x < 6 and pos_y < 6:
             opponent_found = False
             for _x, _y in zip(range(pos_x + 1, 7, 1), range(pos_y + 1, 7, 1)):
@@ -123,8 +142,11 @@ class Board:
                     else False
                 if opponent_found and self.board[_y][_x] == player_id:
                     return True
+                if self.board[_y][_x] == 0 or self.board[_y][_x] == player_id:
+                    break
 
         # Check south
+        #print('Checking south')
         if pos_y < 6:
             opponent_found = False
             for _y in range(pos_y + 1, 7, 1):
@@ -133,8 +155,11 @@ class Board:
                     else False
                 if opponent_found and self.board[_y][pos_x] == player_id:
                     return True
+                if self.board[_y][pos_x] == 0 or self.board[_y][pos_x] == player_id:
+                    break
 
         # Check south west
+        #print('Checking south west')
         if pos_x > 1 and pos_y < 6:
             opponent_found = False
             for _x, _y in zip(reversed(range(0, pos_x, 1)), range(pos_y + 1, 7, 1)):
@@ -143,18 +168,25 @@ class Board:
                     else False
                 if opponent_found and self.board[_y][_x] == player_id:
                     return True
+                if self.board[_y][_x] == 0 or self.board[_y][_x] == player_id:
+                    break
 
         # Check west
+        #print('Checking west')
         if pos_x > 1:
             opponent_found = False
             for _x in reversed(range(0, pos_x, 1)):
                 opponent_found = True if (opponent_found or
                                           (self.board[pos_y][_x] != 0 and self.board[pos_y][_x] != player_id))\
                     else False
+                print(_x, pos_y, opponent_found, self.board[pos_y][_x])
                 if opponent_found and self.board[pos_y][_x] == player_id:
                     return True
+                if self.board[pos_y][_x] == 0 or self.board[pos_y][_x] == player_id:
+                    break
 
         # Check north west
+        #print('Checking north west')
         if pos_x > 1 and pos_y < 6:
             opponent_found = False
             for _x, _y in zip(reversed(range(0, pos_x, 1)), reversed(range(0, pos_y, 1))):
@@ -163,6 +195,8 @@ class Board:
                     else False
                 if opponent_found and self.board[_y][_x] == player_id:
                     return True
+                if self.board[_y][_x] == 0 or self.board[_y][_x] == player_id:
+                    break
 
         return False
 
--- main.py
+++ main.py
@@ -2,13 +2,20 @@ import socket
 import struct
 import traceback
 import random
+import string
 import sys
+import time
 
 from board import Board
 
 
+def random_generator(size=6, chars=string.ascii_uppercase + string.digits):
+    return ''.join(random.choice(chars) for x in range(size))
+
+
 def bytes_to_int(bytes):
     return int.from_bytes(bytes, byteorder='big')
+
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -19,28 +26,47 @@ sock.connect(server_address)
 BUFFER_SIZE = 64
 
 board = Board()
-board.output()
+#board.output()
+#print(board.get_valid_moves(1))
+#print(board.slot_has_valid_move(5, 4, 2))
+#sys.exit()
 
+game_is_active = True
 try:
-    sock.send(b'Bot #1')
+    name = random_generator()
+    print('Bot name:', name)
+    sock.send(bytes(name, 'utf-8'))
     data = sock.recv(1)
     player_id = bytes_to_int(data)
     print('Received the id', player_id)
-    data = sock.recv(BUFFER_SIZE)
-    unpacked = struct.unpack('17c', data)
-    board.update(unpacked[1:])
-    gameState = unpacked[:1]
-    valid_moves = board.get_valid_moves(player_id)
-    print(valid_moves)
-    print('next move:', random.choice(valid_moves))
-    #print(int.from_bytes(unpacked[7], byteorder='big'))
-    print('Sending next move')
-    sock.send(struct.pack('c', b'\x32'))
-    data = sock.recv(1)
-    if bytes_to_int(data) == 1:
-        print('good move')
-    else:
-        print('bad move')
+
+    while game_is_active:
+        data = sock.recv(BUFFER_SIZE)
+        unpacked = struct.unpack('17c', data)
+        board.update(unpacked[1:])
+        gameState = unpacked[:1]
+        # Interpret game state
+        my_turn = True
+        while my_turn:
+            valid_moves = board.get_valid_moves(player_id)
+            print(valid_moves)
+            next_move = random.choice(valid_moves)
+            print('Sending next move', next_move)
+            move = 0
+            move += next_move[0] << 4
+            move += next_move[1]
+            print(move)
+            sock.send(struct.pack('c', bytes([move])))
+            data = sock.recv(1)
+            if bytes_to_int(data) == 1:
+                print('good move')
+                my_turn = False
+            else:
+                print('bad move, try another move')
+                board.output()
+                sys.exit()
+            time.sleep(1)
+    print('wtf')
 except Exception as e:
     print(traceback.format_exc())
 finally:
